be889ee1b30c49331d8f0c5aad272bd637589a68
winex01 <winnie131212592@gmail.com>
2022-03-16 08:19:45 +0800

    add db migration source

---
+++ database/migrations/2022_03_16_080850_create_sources_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+
+            // backpack reorder
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->integer('lft')->unsigned()->nullable();
+            $table->integer('rgt')->unsigned()->nullable();
+            $table->integer('depth')->unsigned()->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sources');
+    }
+}
