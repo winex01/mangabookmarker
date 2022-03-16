0cba470598b320245100afd1ef9dbba5d46e91cd
winex01 <winnie131212592@gmail.com>
2022-03-16 09:45:52 +0800

    create mangas migration

---
+++ database/migrations/2022_03_16_094004_create_mangas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMangasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('photo')->nullable();
+            $table->string('title');
+            $table->string('author');
+
+            $table->foreignId('manga_type_id')->constrained();
+
+            $table->text('alternative_name')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mangas');
+    }
+}
