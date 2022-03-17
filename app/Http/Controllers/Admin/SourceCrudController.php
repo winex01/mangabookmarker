16eb3e9d23e34d9f06290ca91a7b529ae478a78b
winex01 <winnie131212592@gmail.com>
2022-03-17 08:12:04 +0800

    fix crud show operation display

--- app/Http/Controllers/Admin/SourceCrudController.php
+++ app/Http/Controllers/Admin/SourceCrudController.php
@@ -58,6 +58,12 @@ class SourceCrudController extends CrudController
         ]);
     }
 
+    protected function setupShowOperation()
+    {
+        $this->crud->set('show.setFromDb', false); // remove fk column such as: gender_id
+        $this->setupListOperation();
+    }
+
     /**
      * Define what happens when the Create operation is loaded.
      * 
